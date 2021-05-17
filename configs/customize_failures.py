cb1c8ef0bb258798e0074632377ef4a3657e4325
AdrianoDee <adriano.diflorio@ba.infn.it>
2021-05-18 01:24:22 +0200

    Update Customise Failures

--- configs/customize_failures.py
+++ configs/customize_failures.py
@@ -1,10 +1,8 @@
 from CondCore.CondDB.CondDB_cfi import *
 
 def customize_alignment(process):
-        
         process.load("CondCore.DBCommon.CondDBCommon_cfi")
         process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-        
         import CalibTracker.Configuration.Common.PoolDBESSource_cfi
 
         process.GlobalTag.toGet = cms.VPSet(
@@ -16,22 +14,35 @@ def customize_alignment(process):
                                                                                  tag = cms.string("TrackerAlignmentExtendedErrors_BPIX50_FPIX50_TIB20_TOB20_TID40_TEC40"),
                                                                                  connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),),
                                            )
+        return process
 
 def customize_failures(process,scen="d"):
+	
+	if scen==None:
+           return process
 
-        S = "SiPixelQuality_Phase1FailureScenario_2018" + scen
+	S = "SiPixelQuality_Phase1FailureScenario_2018" + scen
 
-        process.SiPixelQualityDBReader = cms.ESSource("PoolDBESSource",
-                        BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
-                        DBParameters = cms.PSet(
-                                messageLevel = cms.untracked.int32(0),
-                                authenticationPath = cms.untracked.string('')),
+	process.SiPixelQualityDBReader = cms.ESSource("PoolDBESSource",
+				       BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+                                       DBParameters = cms.PSet(
+                                       messageLevel = cms.untracked.int32(0),
+                                       authenticationPath = cms.untracked.string('')),
+                                       connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
+                                              toGet = cms.VPSet(
+                                                  cms.PSet(
+                                                      record = cms.string('SiPixelQualityFromDbRcd'),
+						      tag = cms.string(S),
+                                                  ),
+                                                  cms.PSet(
+	                                              record = cms.string('SiPixelQualityFromDbRcd'),
+		                                      label = cms.untracked.string('forDigitizer'),
+						      tag = cms.string(S)
+                                                  ),
+                                              )
+                                          )
 
-                        connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
-                        toGet = cms.VPSet(
-                                        cms.PSet(record = cms.string('SiPixelQualityFromDbRcd'),
-                                                tag = cms.string(S),))
-                                        )
+	process.es_prefer_Quality = cms.ESPrefer("PoolDBESSource","SiPixelQualityDBReader")
 
-        process.es_prefer_Quality = cms.ESPrefer("PoolDBESSource","SiPixelQualityDBReader")
+	return process
 
